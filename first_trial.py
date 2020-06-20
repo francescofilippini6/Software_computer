bd72a518bbee6d0099dbf82ec635eb2fa61433f1
Francesco Filippini <ffilippi@cca008.in2p3.fr>
2020-06-20 19:12:57 +0200

    In FileBiner function 10 ns binning

--- first_trial.py
+++ first_trial.py
@@ -1,12 +1,12 @@
 from orcasong.core import FileBinner
 import numpy as np
+
 bin_edges_list = [
     ["pos_z", np.linspace(0, 700, 19)],
-    ["time", np.linspace(-100, 500, 300)],
+    ["time", np.linspace(-100, 1000, 111)],
 ]
 
 fb = FileBinner(bin_edges_list) #,det_file="KM3NeT_00000042_00008494.detx")
-infiles=['/sps/km3net/users/ffilippi/ML/test.h5','/sps/km3net/users/ffilippi/ML/numuCC.h5']
-#fb.run('test.h5','outfile3333.h5')
+infiles=['/sps/km3net/users/ffilippi/ML/mupage.h5','/sps/km3net/users/ffilippi/ML/numuCC.h5']
 fb.run_multi(infiles,'/sps/km3net/users/ffilippi/ML/outputfolder',True)
 
