c773f52b29747f051866e0960c19a69283a91855
Francesco Filippini <ffilippi@cca008.in2p3.fr>
2020-06-20 19:14:07 +0200

    creation of a new file numuCC_x_y with only x array and y (label)

--- modify_h5_file_adding_label.py
+++ modify_h5_file_adding_label.py
@@ -13,45 +13,46 @@ km3pipe.style.use("km3pipe")
 
 #to read the x group in the h5 file
 def printfile():
-    with h5py.File('numuCC_hist.h5','r') as hdf:
+    with h5py.File('/sps/km3net/users/ffilippi/ML/outputfolder/numuCC_hist.h5','r') as hdf:
         base_items = list(hdf.items())
         print('Items in the base directory', base_items)
         g2 = np.array(hdf.get('x'))
-        print(g2)
+        #print(g2)
         print('shape',g2.shape)
         nevt=g2.shape[0]
         print(nevt)
         ybins=g2.shape[1]
         xbins=g2.shape[2]
+        #taking the first image in g2 array and looking at its rows
+        for u in g2[0]:
+            # print(u)
+            print (max(u))
     return g2
     
-    #to plot the image in 3D histo
-
-    #for i in range(0,nevt):
-    #    imgplot = plt.imshow(g2[i],cmap="viridis")
-    #    plt.colorbar()
-    #    name='image'+str(i)+'done.jpg'
-    #    plt.savefig(name)
-    #    print(name)
-
-def appendLabelDataset():
-    with h5py.File('numuCC_hist.h5', 'a') as hdf:
+def appendLabelDataset(filename):
+    with h5py.File('/sps/km3net/users/ffilippi/ML/outputfolder/'+filename,'a') as hdf:
         g2 = np.array(hdf.get('x'))
         nevt=g2.shape[0]
         yl=np.ones(nevt)
         hdf.create_dataset('y', data=yl, dtype=yl.dtype)
 
 def newfile(g2):
-    #to write the labels in a path file
+    """
+    Normalization of the g2 ndarray.
+    Can be applied to the original file, appending the normalized one, or can bw called on the newfie function
+    appending howevert the normalized one
+    """ 
     nevt=g2.shape[0]
     yl=np.ones(nevt)
-    xl=g2
+    xl=np.divide(g2, 255)
+    print(xl)
     print (len(yl))
-    path='numuCC_x_y.h5'
+    path='/sps/km3net/users/ffilippi/ML/outputfolder/numuCC_x_y.h5'
     with h5py.File(path, 'w') as hdf:
         hdf.create_dataset('x', data=xl, dtype=xl.dtype)
         hdf.create_dataset('y', data=yl, dtype=yl.dtype)
 
-#ci=printfile()
-#newfile(ci)
-appendLabelDataset()
+
+ci=printfile()
+newfile(ci)
+#appendLabelDataset()
