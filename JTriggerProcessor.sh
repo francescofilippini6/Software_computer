104bb2a41eba3ea8588cb9b9b519506aaf446fb0
Francesco Filippini <ffilippi@cca010.in2p3.fr>
2020-07-08 18:11:27 +0200

    Script developed inside Jpp to produce random noise from K40

---
+++ JTriggerProcessor.sh
@@ -0,0 +1,93 @@
+#!/bin/zsh
+#
+# \author mdejong
+#
+version=1.0
+script=${0##*/}
+
+# ------------------------------------------------------------------------------------------
+#
+#                         Utility script to test JTriggerProcessor.
+#
+# ------------------------------------------------------------------------------------------
+
+
+if [ -z $JPP_DIR ]; then
+    echo "Variable JPP_DIR undefined."
+    exit
+fi
+
+source $JPP_DIR/setenv.sh $JPP_DIR
+
+set_variable  DEBUG          2
+set_variable  WORKDIR        /sps/km3net/users/ffilippi/ML/random_noise
+set_variable  DETECTOR       $JPP_DATA/km3net_reference.detx
+set_variable  TRIGGER        $JPP_DATA/trigger_parameters_arca.txt
+set_variable  BACKGROUND_HZ  10e3 600 60 7 0.8 0.08
+set_variable  RECYCLING      10 100e3
+set_variable  numberOfSlices 100
+
+if ( do_usage $* ); then
+    usage "$script [detector file [trigger file [working directory]]]"
+fi
+
+case $# in
+    3) set_variable WORKDIR  $3;&
+    2) set_variable TRIGGER  $2;&
+    1) set_variable DETECTOR $1;;
+esac
+
+set_variable  INPUT_FILE     $WORKDIR/timeslice.root
+set_variable  OUTPUT_FILE    $WORKDIR/trigger_processor.root
+
+if [[ ! -f $DETECTOR ]]; then
+    JDetector.sh $DETECTOR
+fi
+
+if ( ! reuse_file $INPUT_FILE ); then
+
+    echo "Generating random background."
+
+    print_variable     DETECTOR INPUT_FILE BACKGROUND_HZ
+    check_input_file   $DETECTOR
+    check_output_file  $INPUT_FILE
+
+    timer_start
+
+    JRandomTimesliceWriter \
+	-a $DETECTOR                  \
+	-o $INPUT_FILE                \
+	-n $numberOfSlices            \
+	-B "$BACKGROUND_HZ"           \
+	-N "$RECYCLING"               \
+	-d $DEBUG                          
+
+    timer_stop
+    timer_print
+
+fi
+
+if ( ! reuse_file $OUTPUT_FILE ); then
+
+    echo "Processing data."
+
+    print_variable     DETECTOR TRIGGER INPUT_FILE OUTPUT_FILE
+    check_input_file   $DETECTOR $TRIGGER $INPUT_FILE
+    check_output_file  $OUTPUT_FILE
+
+    timer_start
+
+    JTriggerProcessor \
+	-a $DETECTOR                  \
+	-f $INPUT_FILE                \
+	-o $OUTPUT_FILE               \
+	-@ $TRIGGER                   \
+	-C JDAQTimesliceL0            \
+	-d $DEBUG
+  
+    timer_stop
+    timer_print
+
+fi
+
+JPrintTree -f $OUTPUT_FILE
