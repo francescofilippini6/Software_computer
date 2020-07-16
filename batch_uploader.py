eb8944daa6cdde107848872a866bf29af98604ae
Francesco Filippini <ffilippi@cca009.in2p3.fr>
2020-07-16 12:44:31 +0200

    imporvements on memory usage

--- batch_uploader.py
+++ batch_uploader.py
@@ -3,6 +3,9 @@ import h5py
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 import keras
+from memory_profiler import profile
+
+
 
 class DataGenerator(keras.utils.Sequence) :
   def __init__(self, list_IDs, labels, batch_size=32, dim=(18,280,31), n_channels=1,n_classes=2, shuffle=True):
@@ -14,17 +17,10 @@ class DataGenerator(keras.utils.Sequence) :
     self.n_classes = n_classes
     self.shuffle = shuffle
     self.on_epoch_end()  
-    #def __init__(self, X_image, labels, batch_size) :
-    #self.image = X_image
-    #self.labels = labels
-    #self.batch_size = batch_size
-    
     
   def __len__(self):
     'Denotes the number of batches per epoch'
-    batches=int(np.floor(len(self.list_IDs) / self.batch_size))
-    #print("len",batches)
-    return (batches)
+    return int(np.floor(len(self.list_IDs) / self.batch_size))
     
     
   def __getitem__(self, index):
@@ -35,7 +31,7 @@ class DataGenerator(keras.utils.Sequence) :
     
     # Find list of IDs
     list_IDs_temp = [self.list_IDs[k] for k in indexes]
-    #print("listIDs",list_IDs_temp)
+    
     # Generate data
     X, y = self.__data_generation(list_IDs_temp)
     
@@ -47,28 +43,38 @@ class DataGenerator(keras.utils.Sequence) :
     self.indexes = np.arange(len(self.list_IDs))
     if self.shuffle == True:
       np.random.shuffle(self.indexes)
-
+  
+  #@profile
   def __data_generation(self, list_IDs_temp):
     'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
     # Initialization
     X = np.empty((self.batch_size, *self.dim, self.n_channels))
     y = np.empty((self.batch_size), dtype=int)
     filepath='/sps/km3net/users/ffilippi/ML/outputfolder_mupage/concatenated_2.h5'
-    a = h5py.File(filepath, "r")
-    info1 = a['x']
-    info2= a['y']
-    counter=0
-    partial=[]
+    fillo = h5py.File(filepath, "r")
+    info1 = fillo['x']
+    info2 = fillo['y']
+    counter = 0
+    #partial=[]
+    #print(len(list_IDs_temp))
     for a in list_IDs_temp:
-      partial=np.array(info1['group_id'==a])
+      #print(counter)
+      X[counter,]=(np.array(info1['group_id'==a])).reshape(18,280,31,1)
       #print('partial',partial)
-      partial=partial.reshape(18,280,31,1)
-      X[counter,]=partial
-      partial2= np.array(info2['group_id'==a])
-      y[counter]=partial2.reshape(1)
+      #X[counter,]=X[counter,].reshape(18,280,31,1)
+      #X[counter,]=partial
+      #print (len(X[0][0][0][0]))
+      y[counter]= (np.array(info2['group_id'==a])).reshape(1)
+      #y[counter]= y[counter].reshape(1)
+      #y[counter] = partial2
+      #print(y[counter])
       counter+=1
-    return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+    fillo.close()
+    return X, y
 
+  def release_list(self,a):
+    del a[:]
+    del a
 
 if __name__=="__main__":
   ci=DataGenerator()
