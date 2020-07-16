78587e94fd3eae69da5907d5517688cc9f094d0d
Francesco Filippini <ffilippi@cca003.in2p3.fr>
2020-07-16 10:41:10 +0200

    DataGenerator modify. Custom for my file configuration

--- batch_uploader.py
+++ batch_uploader.py
@@ -3,6 +3,7 @@ import h5py
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 import keras
+
 class DataGenerator(keras.utils.Sequence) :
   def __init__(self, list_IDs, labels, batch_size=32, dim=(18,280,31), n_channels=1,n_classes=2, shuffle=True):
     self.dim = dim
@@ -21,7 +22,9 @@ class DataGenerator(keras.utils.Sequence) :
     
   def __len__(self):
     'Denotes the number of batches per epoch'
-    return int(np.floor(len(self.list_IDs) / self.batch_size))
+    batches=int(np.floor(len(self.list_IDs) / self.batch_size))
+    #print("len",batches)
+    return (batches)
     
     
   def __getitem__(self, index):
@@ -32,7 +35,7 @@ class DataGenerator(keras.utils.Sequence) :
     
     # Find list of IDs
     list_IDs_temp = [self.list_IDs[k] for k in indexes]
-    
+    #print("listIDs",list_IDs_temp)
     # Generate data
     X, y = self.__data_generation(list_IDs_temp)
     
@@ -50,14 +53,22 @@ class DataGenerator(keras.utils.Sequence) :
     # Initialization
     X = np.empty((self.batch_size, *self.dim, self.n_channels))
     y = np.empty((self.batch_size), dtype=int)
-    
-    # Generate data
-    for i, ID in enumerate(list_IDs_temp):
-      # Store sample
-      #X[i,] = np.load('outputfolder_neutrino/' + str(ID) + '.npy')
-      #X[i,] = np.array(
-      df = pd.read_hdf('outputfolder_neutrino/concatenated_2.h5', 'cleanuserbase', start=0, stop=10)
-      # Store class
-      y[i] = self.labels[ID]
-      
-      return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+    filepath='/sps/km3net/users/ffilippi/ML/outputfolder_mupage/concatenated_2.h5'
+    a = h5py.File(filepath, "r")
+    info1 = a['x']
+    info2= a['y']
+    counter=0
+    partial=[]
+    for a in list_IDs_temp:
+      partial=np.array(info1['group_id'==a])
+      #print('partial',partial)
+      partial=partial.reshape(18,280,31,1)
+      X[counter,]=partial
+      partial2= np.array(info2['group_id'==a])
+      y[counter]=partial2.reshape(1)
+      counter+=1
+    return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
+
+
+if __name__=="__main__":
+  ci=DataGenerator()
