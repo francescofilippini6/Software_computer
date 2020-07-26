2ef4afcc8a0bb9b66747f045372c7208e04c1290
Francesco Filippini <ffilippi@cca002.in2p3.fr>
2020-07-27 00:21:54 +0200

    script to download the complete files from google drive

---
+++ downloader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd outputfolder_mupage
+rm concatenated.h5
+
+fileid_muon="1fTd_EeBTuzjqiVJsSt2cBNvKRvuDlw6k"
+filename_muon="concatenated.h5"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid_muon}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid_muon}" -o ${filename_muon}
+rm cookie
+cd ..
+
+cd outputfolder_neutrino
+rm concatenated.h5
+
+fileid_neutrino="1V61tD0KuL4D4uyWeNyb5972C9fHpjfU4"
+filename_neutriino="concatenated.h5"
+curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid_neutrino}" > /dev/null
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid_neutrino}" -o ${filename_neutrino}
+rm cookie
+cd ..
